e9ea0f81a9fa25843d34614f5c449728231b9851
Clint Sharp <clint@typhoon.org>
2018-12-05 01:39:56 +0000

    adding ELK

--- gogen-filebeat/entrypoint.sh
+++ gogen-filebeat/entrypoint.sh
@@ -8,7 +8,7 @@ if [ "$1" = "start-kafka" ]; then
     mkdir -p /opt/be/log
     gogen -v -cd /etc/gogen -o file --filename /opt/be/log/transaction.log gen -s businessevent > /var/log/gogen/transaction.log 2>&1 &
     gogen -v -cd /etc/gogen -o file --filename /opt/be/log/auth.log gen -s authfailed > /var/log/gogen/authfailed.log 2>&1 &
-    filebeat -c /etc/filebeat/filebeat.yml run
+    filebeat -c /etc/filebeat/filebeat-kafka.yml run
 elif [ "$1" = "start-direct" ]; then
     sleep 15
     filebeat -c /etc/filebeat/filebeat.yml run &
