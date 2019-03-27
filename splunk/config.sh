738114d506d4a7a3e3dbdd9da829f051de7a4fc7
Clint Sharp <clint@typhoon.org>
2019-03-27 13:08:33 -0700

    Numerous changes:
    * Support for suppress & parsing functions
    * More routing examples
    * Fix a few datagen bugs

--- splunk/config.sh
+++ splunk/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -n "$CRIBL_ROUTING_DEMO" ]; then
-	cat <<-EOF > $SPLUNK_HOME/etc/apps/cribl/local/inputs.conf
+	cat <<-EOF >> $SPLUNK_HOME/etc/apps/cribl/local/inputs.conf
 		[splunktcp://9997]
 		connection_host = ip
 	EOF
